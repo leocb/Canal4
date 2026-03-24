e02274183dc1261a08b8f07af723dc74b9b405dd
leocb <github@leobottaro.com>
2026-03-23 21:50:06 -0300

    add vite allowedhosts env

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,17 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  envPrefix: ['VITE_', 'SPACETIMEDB_'],
-  server: {
-    host: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-      },
-    }
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const allowedHosts = env.VITE_ALLOWED_HOSTS ? env.VITE_ALLOWED_HOSTS.split(',') : [];
+
+  return {
+    plugins: [react()],
+    envPrefix: ['VITE_', 'SPACETIMEDB_'],
+    server: {
+      host: true,
+      allowedHosts: allowedHosts,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3001',
+          changeOrigin: true,
+        },
+      }
+    },
+  };
 })
